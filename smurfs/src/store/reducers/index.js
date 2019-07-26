378d333e81c32689e97e610ad2f40651d68e35ce
Clark Williams <theclarkwilliams@gmail.com>
2019-07-26 09:36:14 -0600

    first version will be with redux. set up initial state and reducer, added thunk and spinner

---
+++ smurfs/src/store/reducers/index.js
@@ -0,0 +1,13 @@
+export const initialState = {
+    gettingSmurf: false,
+    error: "",
+    smurf: ""
+}
+
+export const reducer = (state = initialState, action) => {
+    switch(action.type) {
+        default:
+            return state;
+        
+    }
+}
